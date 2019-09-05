299caacdd32334e3ad92e34245d5a933ecb44757
Tiaan du Plessis <tiaanduplessis@hotmail.com>
2019-09-05 13:09:24 +0200

    Update translate input

--- src/components/utils.js
+++ src/components/utils.js
@@ -4,5 +4,14 @@ import hoistStatics from 'hoist-non-react-statics'
 export const mapProps = map => Component =>
   hoistStatics(props => <Component {...map(props)} />, Component)
 
-export const errorForField = (errors, touched, fieldname) =>
-  errors[fieldname] && touched[fieldname] ? errors[fieldname] : ''
+export const errorForField = (errors, touched, fieldname) => {
+  if (errors[fieldname] && touched[fieldname]) {
+    if (Array.isArray(errors[fieldname]) && errors[fieldname].length > 0) {
+      return errors[fieldname][0].value || errors[fieldname][0]
+    }
+
+    return errors[fieldname] || ''
+  }
+
+  return ''
+}
