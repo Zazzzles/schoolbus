c4aa2e34c9acd495324212b2e3f57057ebb05ab9
gary <ggeorgeu@fullfacing.com>
2020-09-04 13:36:17 +0200

    added clear to story

--- src/components/icon-button/index.stories.js
+++ src/components/icon-button/index.stories.js
@@ -22,7 +22,7 @@ Base.args = {
 export const variant = () => (
   <>
     <Text mb='1em'>
-      variant <em>(E.G. primary, secondary, light, danger)</em>
+      variant <em>(E.G. primary, secondary, light, clear, danger)</em>
     </Text>
     <Flex m='.5em 0 2em'>
       <IconButton variant='primary' mr='1em' />
